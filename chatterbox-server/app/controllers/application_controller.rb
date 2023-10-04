e4ad9605f26f7cebc47dc9ed55631a54a06f7032
Megan <esler6724@gmail.com>
2023-10-03 19:10:13 -0700

    commit tree f6913fac60f439ea5c56fe2d4f94f3f7e56c43ac

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,25 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
+
+  post '/messages' do
+    messages = Message.create(
+      body: params[:body],
+      username: params[:username],
+      created_at: [:created_at],
+      updated_at: [:updated_at]
+    )
+    # body: string
+    # username: string
+    # created_at: timestamp
+    # updated_at: timestamp
+    messages.to_json
+  end
+
+  get '/messages' do
+    messages = Message.allows
+    messages.to_json
+  end
   
 end
+
